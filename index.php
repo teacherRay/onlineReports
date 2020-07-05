916d3899dfa4726f9df65239bd10aae2d16ece2a
teacherRay <34903301+teacherRay@users.noreply.github.com>
2020-07-05 10:57:35 +0700

    Added row names to tables

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                 <div class="row">
                     <div class="col-md-12 jumbotron">
                             <h2 style="text-align: center;">
-                                Welcome to the Home of Engine Online Report System
+                                Welcome to the Home of Engine Online Report Card System
                                 <span style="float: right;"><a href="login.php" class="btn btn-info btn-lg">Admin Login</a></span>
                             </h2>
                     </div>
@@ -20,9 +20,10 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-12 jumbotron">
-                            <h2>Student's information</h2>
+                            <h2>Select a Classroom and Classtime</h2>
                             <form action="index.php" method="post">
-                <input type="text" name="studentid" placeholder="Enter Student ID" style="width: 240px;height: 35px;"><span>OR/AND<span>
+                <!-- <input type="text" name="classtime" placeholder="Enter Am or Pm" style="width: 140px;height: 35px;"><span> AND <span> -->
+
                  <select name="classroom" class="btn btn-info" >
                                     <option>SELECT CLASSROOM</option>
                                     <option>101i</option>
@@ -31,6 +32,7 @@
                                     <option>104i</option>
                                     <option>105i</option>
                                 </select>
+                <input type="text" name="classtime" placeholder="Am or Pm" style="width: 70px;height: 35px;">                                
                   <input type="submit" name="show" value="SHOW INFO" class="btn btn-success text-center" style="margin-left: 30px;" >  
                             </form>
                         </div>
@@ -41,20 +43,29 @@
 <table class="table table-striped table-bordered table-responsive text-center">
     <tr >
         <th class="text-center">Student ID.</th>
-        <th class="text-center">Classroom</th>
         <th class="text-center">Full Name</th>
+        <th class="text-center">Classroom</th>
+        <th class="text-center">Classtime</th>
+        <th class="text-center">Behaviour</th>
+        <th class="text-center">Comprehension</th>
+        <th class="text-center">Participation</th>
+        <th class="text-center">Conversation</th>
+        <th class="text-center">Homework</th>
+
        
-        <th class="text-center">parent phone No.</th>
-        <th class="text-center">Profile Pic</th>
+        <!-- <th class="text-center">parent phone No.</th> -->
+        <!-- <th class="text-center">Profile Pic</th> -->
     </tr>
 <?php 
     include('dbcon.php');
     if (isset($_POST['show'])) {
 
         $Classroom = $_POST['classroom'];
-        $Studentid = $_POST['studentid'];
+        $Classtime = $_POST['classtime'];
+        // $Studentid = $_POST['studentid'];
 
-        $sql = "SELECT * FROM `student` WHERE `classroom` = '$Classroom' OR `studentid`='$Studentid'";
+        // $sql = "SELECT * FROM `student` WHERE `classroom` = '$Classroom' OR `studentid`='$Studentid'";
+        $sql = "SELECT * FROM `student` WHERE `classroom` = '$Classroom' AND `classtime`='$Classtime'";
 
         $result = mysqli_query($conn,$sql);
         if (mysqli_num_rows($result)>0) {
@@ -62,18 +73,30 @@
                 $Id = $DataRows['id'];
                 $Studentid = $DataRows['studentid'];
                 $Name = $DataRows['name'];
-              
-                // $Pcontact = $DataRows['pcontact'];
                 $Classroom= $DataRows['classroom'];
-                $ProfilePic = $DataRows['image'];
+                $Classtime= $DataRows['classtime'];
+                $Behaviour= $DataRows['behaviour'];
+                $Comprehension= $DataRows['comprehension'];
+                $Participation= $DataRows['participation'];
+                $Conversation= $DataRows['conversation'];
+                $Homework= $DataRows['homework'];
+
+                $Classroom = $_POST['classroom'];
+                $Classtime = $_POST['classtime'];
+                
                 ?>
                 <tr>
                     <td><?php echo $Studentid;?></td>
-                    <td><?php echo $Classroom;?></td>
                     <td><?php echo $Name; ?></td>
-        
-                    <!-- <td><?php echo $Pcontact; ?></td> -->
-                    <td><img src="databaseimg/<?php echo $ProfilePic;?>" alt="img"></td>
+                    <td><?php echo $Classroom;?></td>
+                    <td><?php echo $Classtime;?></td>
+                    <td><?php echo $Behaviour;?></td>
+                    <td><?php echo $Comprehension;?></td>
+                    <td><?php echo $Participation;?></td>
+                    <td><?php echo $Conversation;?></td>
+                    <td><?php echo $Homework;?></td>
+
+
                 </tr>
                 <?php
                 
